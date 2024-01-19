37cbe0263dab909f8f0f10899834d6c5f1b4fb55
fumatchu <47304651+fumatchu@users.noreply.github.com>
2024-01-18 22:58:57 -0500

    Add files via upload

---
+++ install1.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#Install1
+echo " "
+echo " "
+echo "*********************************************"
+echo " "
+echo "This script is a "quick and dirty" to get this server"
+echo "configured as a Samba AD/DC Server. There will be some manual"
+echo "intervention needed later in the install for final configuration."
+echo " "
+echo "What this script does:"
+echo "1. Disable Firewall Services and SELINUX"
+echo "2. Disable un-needed Services"
+echo "2. Install the REPO(s) needed"
+echo "3. Once that is complete we will restart the server"
+echo "4. After the Server restarts, PLEASE LOG BACK IN as root to continue"
+echo " "
+echo "*********************************************"
+echo " "
+echo "This will take around 10-15 minutes depending on your Internet connection"
+echo "and processor speed/memory"
+echo " "
+read -p "Press Enter when you're ready"
+
+
+
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+systemctl disable firewalld
+
+
+
+dnf -y install epel-release
+dnf -y install dnf-plugins-core
+dnf config-manager --set-enabled crb
+dnf -y update
+
+dnf -y install cockpit cockpit-storaged ntsysv wget open-vm-tools
+
+systemctl enable cockpit.socket
+
+dnf -y install docbook-style-xsl gcc gdb gnutls-devel gpgme-devel jansson-devel keyutils-libs-devel krb5-workstation lib
+acl-devel libaio-devel libarchive-devel libattr-devel libblkid-devel libtasn1 libtasn1-tools libxml2-devel libxslt lmdb-
+devel openldap-devel pam-devel perl perl-ExtUtils-MakeMaker perl-Parse-Yapp popt-devel python3-cryptography python3-dns
+python3-gpg python3-devel readline-devel rpcgen systemd-devel tar zlib-devel flex bison dbus-devel python3-markdown bind
+-utils
+
+export PERL_MM_USE_DEFAULT=1
+cpan JSON
+echo "export PATH=/usr/local/samba/bin/:/usr/local/samba/sbin/:$PATH" >> /root/.bash_profile
+
+echo "/root/ADDCInstaller/install2" >> .bash_profile
+
+systemctl disable iscsi
+systemctl disable iscsi-onboot
+
+reboot
---
+++ install2.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+#Install2
+IP=$(hostname -I)
+FQDN=$(hostname)
+DOMAIN=$(hostname)
+host
+echo " "
+echo " "
+echo "*********************************************"
+echo " "
+echo "THIS IS PART TWO OF THE SCRIPT TO INSTALL SAMBA AD/DC"
+echo " "
+echo " "
+echo " "
+echo "What this script does:"
+echo "1. Downloads and compiles the samba latest from samba.org"
+echo "2. Will provision the Samba AD/DC"
+echo "3. Setup krb5 from the Samba install"
+echo "4. Create the samba-ad-dc service for boot"
+echo "5. PLEASE STAY AT THE CONSOLE-THIS IS INTERACTIVE"
+echo " "
+echo "*********************************************"
+echo " "
+echo "This will take around 15-20 minutes depending on your Internet connection"
+echo "and processor speed/memory"
+echo " "
+read -p "Press Enter when you're ready"
+
+cd /root/
+mkdir samba-latest
+wget https://download.samba.org/pub/samba/samba-latest.tar.gz
+
+tar --strip-components=1 -zxvf samba-latest.tar.gz -C /root/samba-latest
+
+cd /root/samba-latest
+./configure
+
+make -j 8
+
+
+make install
+
+echo "************************************"
+echo "The Next step will provision the domain"
+echo "If you setup the server originally with the domain name you want,"
+echo "You just need to input your password...Otherwise,"
+echo "Change the following entries to what you want.. Realm/Domain"
+echo "Server Role should be dc (Default)"
+echo "Keep DNS backend to SAMBA_INTERNAL"
+echo "************************************"
+
+read -p "Press Enter when you're ready"
+
+#Provision the Domain
+samba-tool domain provision --use-rfc2307 --interactive
+
+
+#Create KDC:
+\cp -rf /usr/local/samba/private/krb5.conf /etc/krb5.conf
+
+
+#Move the AD Service and enroll
+\cp /root/ADDCInstaller/samba-ad-dc.service /etc/systemd/system/
+
+
+
+echo "It looks like your Main IP address is:"
+echo "$IP"
+echo "This should be the IP address that you make as your primary DNS"
+echo "REMOVE ALL OTHER DNS ENTRIES!"
+echo "IT SHOULD ONLY BE THE MAIN IP OF THIS SERVER"
+echo "The next page will take you there to modify it"
+echo "Make sure you update this, otherwise AD Registration will fail"
+
+#change domain resolution
+nmtui
+
+systemctl restart NetworkManager
+
+echo "**********************"
+echo "Now we are going to enable and start samba-ad-dc Service"
+echo "**********************"
+
+#Start the AD Service
+systemctl enable samba-ad-dc.service
+systemctl start samba-ad-dc
+
+
+echo "Clean up our mess"
+
+#clean up our mess
+sed -i '$ d' /root/.bash_profile
+rm -f /root/samba-latest.tar.gz
+rm -r -f /root/samba-latest/
+
+
+echo "Now we are going to do some testing"
+
+read -p "Press any Key"
+
+host -t SRV _ldap._tcp..
+host -t SRV _kerberos._udp.test.int.
+host -t A dc.test.int.
