2e98873b6e8aa7e7424879dcf1a8f100d389d93d
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-01-24 12:10:44 -0500

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -245,6 +245,9 @@ tput setaf 1; echo "reboot"; tput sgr0
 
 #Add support for FreeRADIUS
 sed -i '9i \       \ ntlm auth = mschapv2-and-ntlmv2-only' /usr/local/samba/etc/smb.conf
+#Allow plain LDAP binds
+sed -i '10i \       \#ldap server require strong auth = no #UNCOMMENT THIS IF YOU NEED PLAIN LDAP BIND (non-TLS)' /usr/local/samba/etc/smb.conf
+
 #clean up our mess
 sed -i '$ d' /root/.bash_profile
 rm -f /root/samba-latest.tar.gz
