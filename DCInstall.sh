9f7f4b281023b87aae05157cc1c2e01676e1fec3
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-02-23 11:24:04 -0500

    Update DCInstall.sh

--- DCInstall.sh
+++ DCInstall.sh
@@ -147,8 +147,9 @@ clear
 cat <<EOF
 ${GREEN}DHCP Server Setup${TEXTRESET}
 ${RED} Only a scope is being created. DHCP is DISABLED by default${TEXTRESET}
-You can enable it with Server Management after 
-the install if you want to use it
+You can enable it with Server Management after install if needed
+
+
 EOF
 
 read -p "Please provide the beginning IP address in the lease range (based on the network $SUBNETNETWORK): " DHCPBEGIP
