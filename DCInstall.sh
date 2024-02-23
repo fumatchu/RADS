dbfd0d148af0509021b4627185a650ff0c5e59f8
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-02-23 09:23:22 -0500

    Update DCInstall.sh

--- DCInstall.sh
+++ DCInstall.sh
@@ -311,7 +311,7 @@ EOF
 
 #Run tests to validate Samba Install 
 cat <<EOF
-${GREEN} Validation ${TEXTRESET}
+${GREEN}Validation${TEXTRESET}
 The server will now test 
   -Kerberos (Ticket)
   -DNS SRV Records 
