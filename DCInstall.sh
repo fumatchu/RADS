1ea4001c9bf93aca231ccc58f539240b51a49b5e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-03-09 23:27:26 -0500

    Update DCInstall.sh

--- DCInstall.sh
+++ DCInstall.sh
@@ -60,7 +60,7 @@ if [ "$DETECTIP" = "ipv4.method:                            auto" ]; then
   echo ${RED}"Interface $INTERFACE is using DHCP${TEXTRESET}"
   read -p "Please provide a static IP address in CIDR format (i.e 192.168.24.2/24): " IPADDR
   read -p "Please provide a default gateway address: " GW
-  read -p "Please provide the FQDN of this machine: " HOSTNAME
+  read -p "Please provide the FQDN for this machine: " HOSTNAME
   read -p "Please provide the domain search: " DNSSEARCH
   read -p "Please provide an upstream DNS IP for resolution: " DNSSERVER
   clear
