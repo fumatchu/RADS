21d9dbd46207ca50aa1fc59e97260630078cd2e9
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-01-10 16:33:52 -0500

    Update samba-dnf-update-patch.sh

--- patches/samba-dnf-update-patch.sh
+++ patches/samba-dnf-update-patch.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 echo "This is the patch file"
 read -p "Press Enter"
+
+# Define the file to check for and the URL to download
+CHECK_FILE="samba-dnf-pkg-update"
+DOWNLOAD_URL="https://raw.githubusercontent.com/fumatchu/RADS/main/patches/samba-dnf-update-patch.sh"
+DOWNLOAD_DEST="/root/samba-dnf-update-patch.sh"
+
+# Check if the file exists
+if [ -f "$CHECK_FILE" ]; then
+    # Download the script to the root directory
+    wget -O "$DOWNLOAD_DEST" "$DOWNLOAD_URL"
+    
+    # Change permissions to make it executable
+    chmod 700 "$DOWNLOAD_DEST"
+    
+    # Run the downloaded script
+    "$DOWNLOAD_DEST"
+else
+    echo "File $CHECK_FILE not found. Exiting."
+fi
+rm -f /root/samba-dnf-pkg-update.sh
