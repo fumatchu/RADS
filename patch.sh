1b858c2028c67488d237c6b5a1647d8e9c66037b
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-01-10 16:10:52 -0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#Patch menu
+items=(1 "Patch samba-dnf-pkg-update"
+)
 
+while choice=$(dialog --title "$TITLE" \
+  --backtitle "Patch Update" \
+  --menu "Please select the install type" 15 65 3 "${items[@]}" \
+  2>&1 >/dev/tty); do
+  case $choice in
+  1) /root/ADDCInstaller/patches/patch-samba-dnf-pkg-update ;;
+
+ 
+  esac
+done
+clear # clear after user pressed Cancel
