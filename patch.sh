a781d9bd920d93befb1d044b2b181ef8851f95c9
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-01-13 06:24:20 -0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
-#Patch menu
+#!/bin/bash
+#DC-Installer.sh #Bootstrap to GIT REPO
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+USER=$(whoami)
+MAJOROS=$(cat /etc/redhat-release | grep -Eo "[0-9]" | sed '$d')
+
+#Checking for user permissions
+if [ "$USER" = "root" ]; then
+  echo " "
+else
+  echo ${RED}"This program must be run as root ${TEXTRESET}"
+  echo "Exiting"
+fi
+#Checking for version Information
+if [ "$MAJOROS" = "9" ]; then
+  echo " "
+else
+  echo ${RED}"Sorry, but this installer only works on Rocky 9.X ${TEXTRESET}"
+  echo "Please upgrade to ${GREEN}Rocky 9.x${TEXTRESET}"
+  echo "Exiting the installer..."
+  exit
+fi
+
+cat <<EOF
+${GREEN}**************************
+Please wait while we gather some files
+**************************${TEXTRESET}
+
+
+${YELLOW}Installing wget and git${TEXTRESET}
+EOF
+sleep 1
+
+dnf -y install wget git dialog
+
+cat <<EOF
+${YELLOW}*****************************
+Retrieving Files from GitHub
+*****************************${TEXTRESET}
+EOF
+
+sleep 1
+#Clone FR
+mkdir /root/RADSPatch
+
+git clone https://github.com/fumatchu/RADS.git /root/RADSPatch
+
 items=(1 "Patch samba-dnf-pkg-update"
 )
 
