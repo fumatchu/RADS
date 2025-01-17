436a77124781f107fa12280a1b8b67c097b77387
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-01-17 14:20:13 -0500

    Update DC1-Install.sh

--- DC1-Install.sh
+++ DC1-Install.sh
@@ -775,7 +775,17 @@ dnf -y update
 dnf install epel-release createrepo -y
 crb enable
 dnf install mock -y
+
 dnf download samba --source
+# Validate the downloaded RPM file
+if ls /root/samba-*.rpm 1> /dev/null 2>&1; then
+    echo "${GREEN}Samba RPM file found. Continuing with the script...${TEXTRESET}"
+else
+    echo "${RED}The Samba source rpm file did not download correctly. Please check your network settings.${TEXTRESET}"
+    echo "Exiting..."
+    exit 1
+fi
+
 mock -r rocky-"$MAJOROS"-x86_64 --enablerepo=devel --define 'dist .el'"$MAJOROS"'_'"$MINOROS"'.dc' --with dc "$MOCKSMBVER"src.rpm
 mkdir /root/.samba
 cp /var/lib/mock/rocky-"$MAJOROS"-x86_64/result/*.rpm /root/.samba
